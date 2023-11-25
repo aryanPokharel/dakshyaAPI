bf99329e50fa491b251fa658afecd987a0174a68
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-11-25 23:54:14 +0545

    fetchProfileById route added

--- routers/profileRouter.js
+++ routers/profileRouter.js
@@ -15,7 +15,7 @@ mongoose.connect(
 // Protected profile route
 router.get("/", authenticateToken, async (req, res) => {
   try {
-    const profile = await User.findOne({_id : req.user._id});
+    const profile = await User.findOne({ _id: req.user._id });
     if (!profile) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -33,9 +33,9 @@ router.post("/updateProfile", authenticateToken, async (req, res) => {
       password: req.body.password, // You may want to hash the password before saving it
       phone: {
         countryCode: req.body.phone.countryCode,
-        number: req.body.phone.number
+        number: req.body.phone.number,
       },
-      dob: req.body.dob
+      dob: req.body.dob,
     };
 
     const options = { new: true }; // Return the modified document rather than the original
@@ -46,7 +46,32 @@ router.post("/updateProfile", authenticateToken, async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.json({ message: "Profile updated successfully",updatedProfile :  updatedProfile });
+    res.json({
+      message: "Profile updated successfully",
+      updatedProfile: updatedProfile,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/fetchProfileById", authenticateToken, async (req, res) => {
+  try {
+    // to send profile of other user on the basis of received id
+    const found = await User.find({ _id: req.body.userId });
+    if (!found) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const UserToSend = {
+      fullName: found[0]["fullName"],
+      email: found[0]["email"],
+      phone: found[0]["phone"],
+      dob: found[0]["dob"],
+      photo: found[0]["photo"],
+    };
+    res.json(UserToSend);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
